ff1b23ab907f3ae9e8ae5b1ebe046a6dc35a04e2
Charlie Hornsby <charlie.hornsby@hotmail.co.uk>
2021-11-24 21:25:42 +0200

    Use Ubuntu instead of Oracle Linux

--- server.tf
+++ server.tf
@@ -86,7 +86,7 @@ resource "oci_core_subnet" "public" {
   security_list_ids = [oci_core_security_list.rules.id]
 }
 
-// Deploy a single hefty Oracle Linux server and set up the software
+// Deploy a single hefty Ubuntu LTS server and set up the software
 // dependencies and a run script to get started quickly
 resource "oci_core_instance" "server" {
   availability_domain = data.oci_identity_availability_domains.default.availability_domains[0].name
@@ -102,12 +102,12 @@ resource "oci_core_instance" "server" {
     ssh_authorized_keys = file(var.ssh_public_key_path)
   }
   shape_config {
-    memory_in_gbs = 16
+    memory_in_gbs = 24
     ocpus         = 4
   }
-  // https://docs.oracle.com/en-us/iaas/images/image/06a7e282-aff6-45f3-865f-d0bac2435b13/
+  // https://docs.oracle.com/en-us/iaas/images/image/0fef99ff-3e6a-4f32-b504-28143e310475/
   source_details {
-    source_id   = "ocid1.image.oc1.eu-amsterdam-1.aaaaaaaap5yq7hwr66cezuo5drpseaqe65mbcyswmht25j5n4ocghr7jvtma"
+    source_id   = "ocid1.image.oc1.eu-amsterdam-1.aaaaaaaarszpq7ymdbzc7qoultyf4wliee4flk34mw4ux263skueksm7lfxa"
     source_type = "image"
   }
 
@@ -115,21 +115,14 @@ resource "oci_core_instance" "server" {
   //  https://teilgedanken.de/Blog/post/setting-up-a-minecraft-server-using-systemd/
   provisioner "remote-exec" {
     inline = [
-      "sudo yum update --assumeyes",
-      "sudo yum install jdk-16.0.2 --assumeyes",
-      "sudo firewall-cmd --permanent --zone=public --add-port=25565/tcp",
-      "sudo firewall-cmd --permanent --zone=public --add-port=25565/udp",
-      "sudo firewall-cmd --reload",
-      "wget https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar",
-      "echo 'eula=true' > eula.txt",
-      "echo 'java -Xms1G -Xmx4G -jar server.jar nogui' > run.sh",
-      "chmod +x run.sh",
+      "sudo apt-get update",
+      "sudo apt-get upgrade --assume-yes",
     ]
 
     connection {
       host = self.public_ip
       type = "ssh"
-      user = "opc"
+      user = "ubuntu"
     }
   }
 }
