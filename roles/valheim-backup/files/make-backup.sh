a6cbf7b2fc3eea094ba9bca03be6ece266f95676
Charlie Hornsby <charlie.hornsby@hotmail.co.uk>
2022-01-26 21:39:41 +0200

    Document backup script

--- roles/valheim-backup/files/make-backup.sh
+++ roles/valheim-backup/files/make-backup.sh
@@ -3,9 +3,11 @@ set -e
 
 TODAY=$(date --iso-8601 --utc)
 
+# Archive the current game data
 pushd valheim_data
 zip -r "$TODAY" ./* -x ./*.zip
 popd
 
+# Move the archive to the backups folder
 mkdir -p backups
 mv "valheim_data/$TODAY.zip" backups/
